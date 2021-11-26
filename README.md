c1f40320ba9a2eeb678f8fa2d489d55b7165b6c1
Ayoub <ayoubhaddou1@gmail.com>
2021-11-26 02:27:38 +0100

    App finition a faire

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # imdb_app_search
+
+Run app.py to run "Make your Movie" App
+
+Import : 
+Import pandas as pd 
+Import numpy as np 
+import matplotlib.pyplot as plt 
+
--- app_imdb/app_imdb/spiders/app.py
+++ app_imdb/app_imdb/spiders/app.py
@@ -5,9 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 # Fonction pour charger les données :
+
 @st.cache
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.read_csv("datafram.csv", nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
@@ -16,6 +17,8 @@ def load_data(nrows):
 # Import du CSV 
 imdb = pd.read_csv("datafram.csv")
 
+
+# imdb = pd.read_csv('datafram.csv')
 ## --------------  ## -----------------------Déclaration de mes variables : 
 
 # I Convert my Datafram title colomn to list 
@@ -57,20 +60,28 @@ st.subheader('Movies explorer:')
 
 
 ## --------------  ## ----------------------- Déclaaration des inputs :
-# Creation des inputs 
 
+# si le checkbox est sur all alors Datafram complet 
+all = st.sidebar.checkbox("All")
+if all :
+    st.write( "Our all Liberary below! ",imdb)
+
+# Checkbox avec les choix de filtres 
 checkmovie = st.sidebar.checkbox("Filters")
 if checkmovie :
     duration = st.sidebar.select_slider("By duration", time )
-    movie = st.sidebar.multiselect("List of movies", set(list_movies))
+    movie = st.sidebar.multiselect("By movies title", set(list_movies))
     choose_actors = st.sidebar.multiselect('Select your actors',set(list_actors))
     country = st.sidebar.multiselect("Country", set(list_country))
     category = st.sidebar.multiselect('Category',set(list_category))
     language = st.sidebar.multiselect('Language', set(list_language))
     note = st.sidebar.select_slider('By score', score )
-
+    
 # Creation des Df avec les différents filtres ( Par Durée, par films, par acteurs, par pays, par langue ou par catégorie)
-    st.write("By Duration" ,imdb[imdb['duration by min'] <= duration ])
+    
+
+    
+    st.write("By Duration", imdb[imdb['duration by min'] <= duration  ] )
     st.write( "By movies", imdb[imdb["title"].isin(movie)])
     st.write("By actors" , imdb[imdb['actors'].isin(choose_actors)])
     st.write("By Country" , imdb[imdb['Origin country'].isin(country)])
@@ -78,29 +89,37 @@ if checkmovie :
     st.write("By language", imdb[imdb['Origin language'].isin(language)])
     st.write("By score ", imdb[imdb['note'] <= int(note) ])
 
-# si le checkbox est sur all alors Datafram complet 
-all = st.sidebar.checkbox("All")
-if all :
-    st.write( "Our all Liberary below! ",imdb)
-
+# Partie bonus : Modélisation de nos données 
 modelisation = st.sidebar.checkbox("Modelisation")
+
 if modelisation : 
+
+   
+    st.bar_chart(imdb['duration by min'])
+    st.bar_chart(imdb['note'])
+    st.bar_chart(imdb['movie cost'])
     st.subheader("Movies duration by cost :")
     fig, ax = plt.subplots()
     ax.scatter(list(imdb['duration by min']), imdb['movie cost'])
     st.pyplot(fig)
     st.subheader("Movie price by movie score :")
     fig, ax = plt.subplots()
-    ax.scatter(list(imdb['note']), imdb['movie cost'])
+    ax.scatter(imdb['note'], imdb['movie cost'])
     st.pyplot(fig)
     fig, ax = plt.subplots()
     st.subheader("Just training:")
     ax.plot(list(imdb['duration by min']), imdb['movie cost'])
-    st.pyplot(fig)    
+    st.pyplot(fig) 
+
+contact = st.sidebar.checkbox('Others')
+if contact:
+    st.map()
+
+
 #Markdown 
 st.markdown(
 
-    " ###### Made by Ayoub "
+    " ###### Made by Ayoub  "
 )
 
 # data_container = st.container()
